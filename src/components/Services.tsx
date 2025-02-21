7eabd5507711520016f50b9ea0bc3956f566c79d
Uqie Rach <achmadfurqonrachmadie@gmail.com>
2025-02-21 21:01:39 +0700

    improve: add id to layanan section

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -2,7 +2,7 @@ import { services } from "@/utils/constants";
 
 const Service = () => {
   return (
-    <section className="pb-12 pt-20 dark:bg-dark lg:pb-[90px] lg:pt-[120px]">
+    <section className="pb-12 pt-20 dark:bg-dark lg:pb-[90px] lg:pt-[120px]" id="layanan">
       <div className="container mx-auto">
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4">
